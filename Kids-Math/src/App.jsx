a6a6c0ba2791df6f8547096f28d5dd098e63f837
Sarah-ZW <earthclaninfo@gmail.com>
2024-01-24 22:35:31 -0500

    initial commit

---
+++ Kids-Math/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <p>
+        This is going to be an awesome and fun game for kids to play and learn
+        math.
+      </p>
+    </>
+  )
+}
+
+export default App
